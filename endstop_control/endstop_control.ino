41b52fdda0d88475a1abba2425c358c02a745ba7
suneelbelkhale <neelus.genius@gmail.com>
2017-11-09 10:45:50 -0800

    added right ports and tested

--- endstop_control/endstop_control.ino
+++ endstop_control/endstop_control.ino
@@ -11,16 +11,18 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_StepperMotor *belt_stepper = AFMS.getStepper(200, 2);
 
 //TODO: Change these
-const int startStop = 1; 
-const int endStop = 2;
+const int startStop = 3; 
+const int endStop = 12;
 
-const int resetButton = 3;
+const int resetButton = 5;
 
 void back() { // go towards the start stop
-  belt_stepper->onestep(BACKWARD, SINGLE);
+  Serial.println("Stepped back");
+  belt_stepper->onestep(FORWARD, SINGLE);
 }
 void forward() {  // go away from start stop
-  belt_stepper->onestep(FORWARD, SINGLE);
+  Serial.println("Stepped forward");
+  belt_stepper->onestep(BACKWARD, SINGLE);
 }
 
 void release() {
@@ -40,15 +42,18 @@ void setup() {
   AFMS.begin();
   
   Serial.begin(9600);
+  
+  Serial.println("======= STARTING =======");
 
-
+    
   //reset on start
   int sts = digitalRead(startStop);
   while (!sts) {
     back();
-    delay(10);
+    delay(2);
     sts = digitalRead(startStop);
   }
+  Serial.println("-- RESET --");
   release();
 
   location = 0;
@@ -62,27 +67,36 @@ int startStopState = 0;
 void loop() {
   delay(1500);
   
+  
   if (state == 0) {
+    
+    Serial.println("-- Waiting for End stop --");
     int est = digitalRead(endStop);
     while(!est) {
       forward();
-      delay(10);
+      delay(2);
       est = digitalRead(endStop);
     }
+    Serial.println("-- FOUND End stop --");
     release();
     state = 1;
   } 
   else {
-
+  
+    Serial.println("-- Waiting for Start stop --");
     int sts = digitalRead(startStop);
     while(!sts) {
       back();
-      delay(10);
+      delay(2);
       sts = digitalRead(startStop);
     }
+    Serial.println("-- FOUND Start stop --");
     release();
     state = 0;
   }
   
   delay(1500);
+//  Serial.print(digitalRead(startStop));
+//  Serial.print(" ---- ");
+//  Serial.println(digitalRead(endStop));
 }
