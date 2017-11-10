9cf5cbc13e5d2e294443ea72b14a8622660a5dcd
suneelbelkhale <neelus.genius@gmail.com>
2017-11-10 15:07:50 -0800

    demo for milestone

--- endstop_control/endstop_control.ino
+++ endstop_control/endstop_control.ino
@@ -16,6 +16,10 @@ const int endStop = 12;
 
 const int resetButton = 5;
 
+//CHANGE THIS
+const int BATTERY_DISTANCE = 1600;
+const int BATTERY_WAIT_TIME = 5000; //ms
+
 void back() { // go towards the start stop
   Serial.println("Stepped back");
   belt_stepper->onestep(FORWARD, SINGLE);
@@ -43,18 +47,43 @@ void setup() {
   
   Serial.begin(9600);
   
-  Serial.println("======= STARTING =======");
+  Serial.println("======= STARTING DEMO =======");
 
-    
+
+  Serial.println("-- RESETING ON START --");
   //reset on start
   int sts = digitalRead(startStop);
   while (!sts) {
     back();
-    delay(2);
+//    delay(2);
     sts = digitalRead(startStop);
   }
   Serial.println("-- RESET --");
   release();
+
+  delay(2000);
+
+  Serial.println("<<< Going to unload and reload battery >>>");
+  //go unload and reload the battery (this is forward direction)
+  belt_stepper->step(BATTERY_DISTANCE, BACKWARD, SINGLE);
+
+  delay(BATTERY_WAIT_TIME);
+
+  //FINAL RESET
+  Serial.println("-- RESETING ON END --");
+  //reset on start
+  sts = digitalRead(startStop);
+  while (!sts) {
+    back();
+//    delay(2);
+    sts = digitalRead(startStop);
+  }
+  Serial.println("-- RESET --");
+  
+  Serial.println("-  DONE   -");
+  release();
+
+  delay(2000);
 
   location = 0;
   
@@ -65,38 +94,38 @@ int startStopState = 0;
 
 // ALTERNATES between going to end or start stop
 void loop() {
-  delay(1500);
-  
-  
-  if (state == 0) {
-    
-    Serial.println("-- Waiting for End stop --");
-    int est = digitalRead(endStop);
-    while(!est) {
-      forward();
-      delay(2);
-      est = digitalRead(endStop);
-    }
-    Serial.println("-- FOUND End stop --");
-    release();
-    state = 1;
-  } 
-  else {
-  
-    Serial.println("-- Waiting for Start stop --");
-    int sts = digitalRead(startStop);
-    while(!sts) {
-      back();
-      delay(2);
-      sts = digitalRead(startStop);
-    }
-    Serial.println("-- FOUND Start stop --");
-    release();
-    state = 0;
-  }
-  
-  delay(1500);
-//  Serial.print(digitalRead(startStop));
-//  Serial.print(" ---- ");
-//  Serial.println(digitalRead(endStop));
+//  delay(1500);
+//  
+//  
+//  if (state == 0) {
+//    
+//    Serial.println("-- Waiting for End stop --");
+//    int est = digitalRead(endStop);
+//    while(!est) {
+//      forward();
+////      delay(2);
+//      est = digitalRead(endStop);
+//    }
+//    Serial.println("-- FOUND End stop --");
+//    release();
+//    state = 1;
+//  } 
+//  else {
+//  
+//    Serial.println("-- Waiting for Start stop --");
+//    int sts = digitalRead(startStop);
+//    while(!sts) {
+//      back();
+////      delay(2);
+//      sts = digitalRead(startStop);
+//    }
+//    Serial.println("-- FOUND Start stop --");
+//    release();
+//    state = 0;
+//  }
+//  
+//  delay(1500);
+////  Serial.print(digitalRead(startStop));
+////  Serial.print(" ---- ");
+////  Serial.println(digitalRead(endStop));
 }
